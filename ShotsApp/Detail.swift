8f67498ed4e5c0c409e1213a7770646a7d8d17b5
Junne <junne.bai@gmail.com>
2015-10-15 17:58:57 +0800

    add Home page

---
+++ ShotsApp/Detail.swift
@@ -0,0 +1,13 @@
+//
+//  Detail.swift
+//  ShotsApp
+//
+//  Created by Junne on 10/15/15.
+//  Copyright © 2015 Junne. All rights reserved.
+//
+
+import UIKit
+
+class Detail: UIViewController {
+
+}
---
+++ ShotsApp/Home.swift
@@ -0,0 +1,13 @@
+//
+//  Home.swift
+//  ShotsApp
+//
+//  Created by Junne on 10/15/15.
+//  Copyright © 2015 Junne. All rights reserved.
+//
+
+import UIKit
+
+class Home: UIViewController {
+
+}
