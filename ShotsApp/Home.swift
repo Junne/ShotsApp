d8034bdfc0ea88f310a397d723007d8b9172a553
Junne <junne.bai@gmail.com>
2015-10-16 16:47:10 +0800

    complete this demo

--- ShotsApp/Home.swift
+++ ShotsApp/Home.swift
@@ -35,6 +35,15 @@ class Home: UIViewController {
     
     var data = getData()
     var number = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        insertBlurView(backgroundMaskView, style: UIBlurEffectStyle.Dark)
+//        insertBlurView(headerView, style: UIBlurEffectStyle.Dark)
+        animator = UIDynamicAnimator(referenceView: view)
+//        dialogView.alpha = 0
+    }
+    
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(Bool())
         
@@ -56,6 +65,30 @@ class Home: UIViewController {
         
         dialogView.alpha = 1
         
+    }
+    
+    @IBAction func imageButtonDidPress(sender: AnyObject) {
+        springWithCompletion(0.7, animations: {
+            
+            self.dialogView.frame = CGRectMake(0, 0, 600, 800)
+            self.dialogView.layer.cornerRadius = 0
+            self.imageButton.frame = CGRectMake(0, 0, 600, 240)
+            self.likeButton.alpha = 0
+            self.shareButton.alpha = 0
+            self.userButton.alpha = 0
+            self.headerView.alpha = 0
+            
+            }, completion: { finished in
+                self.performSegueWithIdentifier("homeToDetail", sender: self)
+        })
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
+        if segue.identifier == "homeToDetail" {
+            let controller = segue.destinationViewController as! Detail
+            controller.data = data
+            controller.number = number
+        }
     }
     
     @IBAction func shareButtonPress(sender: AnyObject) {
@@ -146,5 +179,65 @@ class Home: UIViewController {
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent
+    }
+    
+    var animator : UIDynamicAnimator!
+    var attachmentBehavior : UIAttachmentBehavior!
+    var gravityBehaviour : UIGravityBehavior!
+    var snapBehavior : UISnapBehavior!
+    
+    @IBOutlet var panRecognizer: UIPanGestureRecognizer!
+    @IBAction func handleGesture(sender: AnyObject) {
+        
+        let myView = dialogView
+        let location = sender.locationInView(view)
+        let boxLocation =  sender.locationInView(dialogView)
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            if snapBehavior != nil {
+                animator.removeBehavior(snapBehavior)
+            }
+            
+            let centerOffset = UIOffsetMake(boxLocation.x - CGRectGetMidX(myView.bounds), boxLocation.y - CGRectGetMidY(myView.bounds))
+            attachmentBehavior = UIAttachmentBehavior(item: myView, offsetFromCenter: centerOffset, attachedToAnchor: location)
+            attachmentBehavior.frequency = 0
+            
+            animator.addBehavior(attachmentBehavior)
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            attachmentBehavior.anchorPoint = location
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            animator.removeBehavior(attachmentBehavior)
+            
+            snapBehavior = UISnapBehavior(item: myView, snapToPoint: view.center)
+            animator.addBehavior(snapBehavior)
+            
+            let translation = sender.translationInView(view)
+            if translation.y > 100 {
+                animator.removeAllBehaviors()
+                let gravity = UIGravityBehavior(items: [dialogView])
+                gravity.gravityDirection = CGVectorMake(0, 100)
+                animator.addBehavior(gravity)
+                
+                delay(0.3) {
+                    self.refreshView()
+                }
+            }
+        }
+        
+    }
+    
+    func refreshView() {
+        number++
+        if number > 3 {
+            number = 0
+        }
+        
+        animator.removeAllBehaviors()
+        
+        snapBehavior = UISnapBehavior(item: dialogView, snapToPoint: view.center)
+        attachmentBehavior.anchorPoint = view.center
+        
+        dialogView.center = view.center
+        viewDidAppear(true)
     }
 }
